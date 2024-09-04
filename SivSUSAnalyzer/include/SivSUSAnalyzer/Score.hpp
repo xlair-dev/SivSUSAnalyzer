44acc7e247e29da8a7e9d46685bbd326f38ee241
Ryoga-exe <git@ryoga.dev>
2024-09-05 07:08:26 +0900

    feat: add Score

---
+++ SivSUSAnalyzer/include/SivSUSAnalyzer/Score.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#define NO_S3D_USING
+#include <Siv3D.hpp>
+
+namespace SivSUSAnalyzer {
+    struct Note {
+        s3d::int32 channel;
+        s3d::int32 lane;
+        s3d::int32 laneType;
+        s3d::int32 measure;
+        s3d::int32 noteType;
+        s3d::int32 tick;
+        s3d::int32 width;
+    };
+
+    struct Score {
+        s3d::int32 measure;
+        s3d::Array<double> bpmList;
+        s3d::Array<Note> tapNotes;
+        s3d::Array<Note> holdNotes;
+    };
+
+    Score ParseScore(const s3d::String& data);
+}
