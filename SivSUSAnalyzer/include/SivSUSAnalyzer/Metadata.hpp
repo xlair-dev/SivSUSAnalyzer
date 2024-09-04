b033bf85726ba53ce00ed5579bef2b884bd9ed48
Ryoga-exe <git@ryoga.dev>
2024-09-05 07:06:15 +0900

    feat: add Metadata

---
+++ SivSUSAnalyzer/include/SivSUSAnalyzer/Metadata.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+#define NO_S3D_USING
+#include <Siv3D.hpp>
+
+namespace SivSUSAnalyzer {
+    struct MetadataEntry {
+        MetadataEntry(const s3d::String& line);
+        s3d::String key;
+        s3d::String value;
+    };
+
+    struct Metadata {
+        s3d::String   title;
+        s3d::String   subtitle;
+        s3d::String   artist;
+        s3d::String   genre;
+        s3d::String   designer;
+        s3d::String   difficulty;
+        s3d::String   playLevel;
+        s3d::String   songID;
+        s3d::FilePath wave;
+        double        waveOffset = 0.0;
+        s3d::FilePath jacket;
+        s3d::FilePath background;
+        s3d::FilePath movie;
+        double        movieOffset = 0.0;
+        double        baseBPM = 60.0;
+        s3d::Array<s3d::String> requests;
+
+        bool SetEntry(const MetadataEntry& entry);
+    };
+
+    Metadata ParseMetadata(const s3d::String& data);
+}
